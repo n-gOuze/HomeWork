ae13f8dd589e5168e17e1bb2e3237fb1f75746bd
Guseva Natalia <natalia.gyseva@gmail.com>
2023-04-07 13:06:41 +0300

    corrected solution

--- FinalProject001/Program.cs
+++ FinalProject001/Program.cs
@@ -1,18 +1,18 @@
 ﻿// Задача: написать программу, которая из имеющуегося массива строк формирует массив из строк,
 // длина которых меньше либо равна 3 символа.
-// Дано: ["hello", "2", "world", ":-)"]     Требуемый результат: ["2", ":-)"]
+// Дано: [hello, 2, world, :-)]     Требуемый результат: [2, :-)]
 
-string[] array = {"hello", "2", "world", ":-)"};
-var result = new string[array.Length];
+string[] myArray = {"hello", "2", "world", ":-)"};
+var newArray = new string[myArray.Length];
 var newSize = 0;
-foreach (var value in array)
+foreach (var value in myArray)
 {
     if (value.Length <= 3)
     {
-        result[newSize] = value;
+        newArray[newSize] = value;
         newSize++;
     }
 }
- 
-Console.WriteLine(string.Join(Environment.NewLine, result, 0, newSize));
+
+Console.WriteLine($"[{string.Join(", ", myArray)}] -> [{string.Join(", ", newArray, 0, newSize)}]");
 
