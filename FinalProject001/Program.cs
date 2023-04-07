a6d5294f3f76dcf80d47f259a6c2476e1826b5fd
Guseva Natalia <natalia.gyseva@gmail.com>
2023-04-07 12:20:32 +0300

    added solution

--- FinalProject001/Program.cs
+++ FinalProject001/Program.cs
@@ -1,1 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача: написать программу, которая из имеющуегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+// Дано: ["hello", "2", "world", ":-)"]     Требуемый результат: ["2", ":-)"]
+
+string[] array = {"hello", "2", "world", ":-)"};
+var result = new string[array.Length];
+var newSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[newSize] = value;
+        newSize++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, newSize));
+
